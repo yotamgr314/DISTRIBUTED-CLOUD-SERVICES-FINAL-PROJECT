eb59edaf01d1c2f6c0617090b40bcad89aad29ae
Yotam Greenstein <yotamgr314@gmail.com>
2025-04-07 04:51:21 +0300

    Create - Signin.js page
    Create - Signin.js page using MUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 // src/App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signUp";
+import SignIn from "./pages/signIn";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signUp" element={<SignUp />} />
+        <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </Router>
   );
